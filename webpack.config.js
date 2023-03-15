70daf00dba8190a583517926ba8693f9f0f85d5f
Nadia Laban <labannadia5@gmail.com>
2023-03-15 07:50:34 +0300

    fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,8 @@ module.exports = {
   ],
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './static'),
-    publicPath: '/static/',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '',
     filename: 'build.js'
   },
   module: {
