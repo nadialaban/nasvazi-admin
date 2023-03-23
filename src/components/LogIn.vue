1d9a7aaed6532448f694397ca9ab8ca43dca1f5a
Nadia Laban <labannadia5@gmail.com>
2023-03-23 23:56:25 +0300

    fix login

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -6,7 +6,10 @@
       </div>
       <div class="card-body">
         <h5 class="card-text label-light">Панель администратора</h5>
-        <div v-if="state === 'number'">
+        <div class="content-container" style="margin-top: 150px" v-if="flags.loading">
+          <loading/>
+        </div>
+        <div v-else-if="state === 'number'">
           <vue-phone-number-input :no-example="true" class="form-shadow" style="border-radius: 4px"
                                   default-country-code="RU" :only-countries="['RU']"
                                   color="#9c4239" valid-color="#7ca474" error-color="#ba1a1a"
@@ -15,7 +18,7 @@
           <small class="card-text error">{{ error }}</small><br>
           <button class="btn btn-primary shadow-none" @click="get_code()">Получить код</button>
         </div>
-        <div v-if="state === 'code'">
+        <div v-else-if="state === 'code'">
           <p class="card-text text-muted">СМС с кодом подтверждения отправлено на номер
             <br>
             +7 {{ number }}.</p>
@@ -36,29 +39,28 @@
 <script>
 import PincodeInput from "vue-pincode-input";
 import axios from "axios";
+import Loading from "./parts/Loading.vue";
 
 export default {
   name: "LogIn",
-  components: {PincodeInput},
+  components: {Loading, PincodeInput},
   data() {
     return {
       state: 'number',
       number: '',
       code: '',
-      userId: 31,
-      error: '\n'
+      userId: undefined,
+      error: '\n',
+      flags: {
+        loading: false
+      }
     }
   },
   computed: {},
   methods: {
     get_code: function () {
-      console.log({
-        phone: '+7 ' + this.number,
-        chatId: '',
-        name: '',
-        expectedRole: 'ADMIN'
-      })
       if (this.phone_re.exec(this.number)) {
+        this.flags.loading = true
         axios
           .post(this.url('/auth/login'), {
             phone: '+7 ' + this.number,
@@ -67,13 +69,15 @@ export default {
             expectedRole: 'ADMIN'
           })
           .then((response) => {
+            console.log(response.data)
             if (response.data.status === 'FAIL') {
               this.error = 'Ошибка доступа'
             } else {
               this.error = ''
-              this.userId = response.data.userId ? response.data.userId : 31
+              this.userId = response.data.userId
               this.state = 'code'
             }
+            this.flags.loading = false
           })
       } else {
         this.error = 'Пожалуйста, проверьте корректность ввода'
@@ -90,6 +94,8 @@ export default {
           .then((response) => {
             if (response.data.status === 'SUCCESS') {
               myEvent.fire('authorised', response.data.user)
+            } else {
+              this.error = 'Неверный код, попробуйте снова'
             }
           })
       } else {
